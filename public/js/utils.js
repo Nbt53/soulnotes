a028af2db92f95dc81aa5d83e10f449b03b04585
nbt53 <nbt853.nh@gmail.com>
2023-06-26 19:17:26 +0100

    video debug 0.5

--- public/js/utils.js
+++ public/js/utils.js
@@ -4,3 +4,48 @@ module.exports.log = (message) => {
     logMessage.textContent = message;
     logOutput.appendChild(logMessage);
   }
+
+
+//   <script>
+//   <div id="log-output"></div>
+// function log(message) {
+//     var logOutput = document.getElementById('log-output');
+//     var logMessage = document.createElement('p');
+//     logMessage.textContent = message;
+//     logOutput.appendChild(logMessage);
+
+// }
+
+// log('hi')
+// var video = document.getElementById('video-element');
+
+// video.addEventListener('loadedmetadata', function (e) {
+//    console.log(e)
+//     log('Video metadata loaded.' + e);
+// });
+
+// video.addEventListener('canplay', function () {
+//     log('Video can start playing.');
+// });
+
+// video.addEventListener('canplaythrough', function () {
+//     log('Video can play through without buffering.');
+// });
+
+// video.addEventListener('error', function () {
+//     log('Error occurred while loading the video.');
+// });
+
+// video.addEventListener('ended', function () {
+//     log('Video playback ended.');
+// });
+
+// video.addEventListener('play', function () {
+//     log('Video playback started.');
+// });
+
+// video.addEventListener('pause', function () {
+//     log('Video playback paused.');
+// });
+
+// </script>
