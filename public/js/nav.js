527f54b155d2da0aff95e7c16acdb10106ae4689
nbt53 <nbt853.nh@gmail.com>
2023-07-01 10:37:50 +0100

    soul notes

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,9 +1,22 @@
 const navBar = document.querySelector('.nav')
 const burger = document.querySelector('.nav__hamburger')
+const burgerContainer = document.querySelector('.nav__hamburger-container')
 const burgerLine1 = document.querySelector('.nav__hamburger-line-1')
 const burgerLine2 = document.querySelector('.nav__hamburger-line-2')
 const burgerLine3 = document.querySelector('.nav__hamburger-line-3')
-const navIcon = document.querySelector('.nav__icon')
+if(document.querySelector('.nav__icon')){
+    const navIcon = document.querySelector('.nav__icon')
+    window.addEventListener('scroll', function() {
+    
+        const scrollPos = window.pageYOffset;
+    if(scrollPos > 0){
+    navIcon.style.display = 'none'
+    }else{
+        navIcon.style.display = 'block'
+    }
+    });
+}
+
 
 let expanded = 0
 
@@ -11,6 +24,8 @@ burger.addEventListener('click', (e) => {
     if (expanded === 0) {
         navBar.style.left = "0"
         expanded = 1
+        burgerContainer.style.height = "100vh"
+       // burgerContainer.addEventListener('click', )
     } else {
         navBar.style.left = "-70%"
         expanded = 0
@@ -34,13 +49,4 @@ burger.addEventListener('click', (e) => {
 
 
 
-window.addEventListener('scroll', function() {
-    
-    const scrollPos = window.pageYOffset;
-if(scrollPos > 0){
-navIcon.style.display = 'none'
-}else{
-    navIcon.style.display = 'block'
-}
-});
 
